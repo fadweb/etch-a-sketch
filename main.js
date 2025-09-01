5d61072fa593ace1146dc2a3684573e5da1d1809
fadwell <fad.web.dev@gmail.com>
2025-09-01 19:20:37 -0400

    created functions to generate grid on user input and change box color on hover

---
+++ main.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".container");
+
+function generateGrid(num) {
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.style.border = "solid black 1px";
+            box.style.flex = `0 0 calc(100% / ${num})`;
+            box.style.aspectRatio = "1 / 1";
+            container.appendChild(box);
+        }
+    }
+}
+
+//changes box background color on hover
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("box")) {
+        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        e.target.style.backgroundColor = randomColor;
+    }
+});
+
+generateGrid(16);
+
+//re-generates grid with user input
+const gridBtn = document.querySelector("#gridBtn");
+gridBtn.addEventListener("click", () => {
+    const newGrid = parseInt(prompt("Enter a number between 2-100"));
+    if (newGrid < 2 || newGrid > 100) {
+        alert("Try Again");
+    } else {
+        container.innerHTML = "";
+        generateGrid(newGrid);
+    }
+});
+
+//clears any background colors on the canvas
+const clearBtn = document.querySelector("#clearBtn");
+clearBtn.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box =>{
+        box.style.backgroundColor = "";
+    })
+})
+
